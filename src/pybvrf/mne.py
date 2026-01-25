a9836fbe67fe0132b9eba36d4a1b82c51a90d56e
Clemens Brunner <clemens.brunner@gmail.com>
2026-01-25 12:33:31 +0100

    Add split parameter to read_raw_bvrf()

--- src/pybvrf/mne.py
+++ src/pybvrf/mne.py
@@ -1,7 +1,7 @@
 from mne import create_info
 from mne.io import BaseRaw, get_channel_type_constants
 
-from pybvrf.pybvrf import read_bvrf
+from pybvrf.pybvrf import read_bvrf, split_participants
 
 
 class RawBVRF(BaseRaw):
@@ -16,8 +16,33 @@ class RawBVRF(BaseRaw):
             Path to the BVRF file (either without extension or one of `.bvrh`, `.bvrd`,
             `.bvrm`, or `.bvri`).
         """
-        header, data, markers, _ = read_bvrf(fname)  # MNE does not support impedances
+        header, data, markers, _ = read_bvrf(fname)
+        self._init_from_data(header, data, markers, *args, **kwargs)
 
+    @classmethod
+    def from_data(cls, header, data, markers, *args, **kwargs):
+        """Create RawBVRF instance from already read data.
+
+        Parameters
+        ----------
+        header : dict
+            Header information.
+        data : ndarray
+            EEG data.
+        markers : ndarray
+            Markers.
+
+        Returns
+        -------
+        RawBVRF
+            The raw data object.
+        """
+        instance = cls.__new__(cls)
+        instance._init_from_data(header, data, markers, *args, **kwargs)
+        return instance
+
+    def _init_from_data(self, header, data, markers, *args, **kwargs):
+        """Initialize from header, data, and markers."""
         fs = header["fs"]
 
         mne_ch_types = list(get_channel_type_constants().keys())
@@ -27,7 +52,7 @@ class RawBVRF(BaseRaw):
         ]
         info = create_info(ch_names=header["ch_names"], sfreq=fs, ch_types=ch_types)
 
-        super().__init__(
+        super(RawBVRF, self).__init__(
             preload=data,
             info=info,
             filenames=[str(header["fname"].with_suffix(".bvrh"))],
@@ -45,7 +70,7 @@ class RawBVRF(BaseRaw):
         )
 
 
-def read_raw_bvrf(fname, *args, **kwargs):
+def read_raw_bvrf(fname, split=False, *args, **kwargs):
     """Read BrainVision Recording Format (BVRF) recording.
 
     Parameters
@@ -53,10 +78,21 @@ def read_raw_bvrf(fname, *args, **kwargs):
     fname : str | Path
         Path to the BVRF file (either without extension or one of `.bvrh`, `.bvrd`,
         `.bvrm`, or `.bvri`).
+    split : bool, optional
+        If True, split multi-participant recordings into separate RawBVRF objects (one
+        per participant). Default is False.
 
     Returns
     -------
-    RawBVRF
-        The raw data.
+    RawBVRF | list of RawBVRF
+        The raw data object (if `split` is False) or a list of raw data objects (if
+        `split` is True).
     """
-    return RawBVRF(fname, *args, **kwargs)
+    header, data, markers, _ = read_bvrf(fname)
+    if split and header["n_participants"] > 1:
+        participants = split_participants(header, data, markers, None)
+        return [
+            RawBVRF.from_data(p_header, p_data, p_markers, *args, **kwargs)
+            for p_header, p_data, p_markers, _ in participants
+        ]
+    return RawBVRF.from_data(header, data, markers, *args, **kwargs)
