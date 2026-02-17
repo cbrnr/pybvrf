5c3bdee7c7f725be843c84b989dc3a4a9f74f602
Clemens Brunner <clemens.brunner@gmail.com>
2026-02-17 14:41:20 +0100

    Add __version__ attribute

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
 ## [UNRELEASED] · YYYY-MM-DD
+### ✨ Added
+- Add `__version__` attribute ([#1](https://github.com/cbrnr/pybvrf/pull/1) by by [Clemens Brunner](https://github.com/cbrnr))
 
 ## [0.1.0] · 2026-02-17
 ### ✨ Added
--- src/pybvrf/__init__.py
+++ src/pybvrf/__init__.py
@@ -2,8 +2,15 @@
 #
 # License: BSD (3-clause)
 
+from importlib.metadata import PackageNotFoundError, version
+
 from pybvrf.export import read_raw_bvrf
 from pybvrf.pybvrf import read_bvrf, read_bvrf_header
 from pybvrf.utils import split_participants
+
+try:
+    __version__ = version("pybvrf")
+except PackageNotFoundError:
+    __version__ = "unknown"
 
 __all__ = ["read_bvrf", "read_bvrf_header", "read_raw_bvrf", "split_participants"]
