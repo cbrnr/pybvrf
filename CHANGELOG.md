c2b4700512ac104e58d594f2c8844329c713d325
Clemens Brunner <clemens.brunner@gmail.com>
2026-02-25 10:25:45 +0100

    More robust loading of schema (#3)
    * More robust loading of schema
    
    * Add changelog entry
    
    * Fix style

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 ### ✨ Added
 - Add `__version__` attribute ([#1](https://github.com/cbrnr/pybvrf/pull/1) by by [Clemens Brunner](https://github.com/cbrnr))
 
+### 🔧 Fixed
+- Use more robust method for accessing package resources ([#3](https://github.com/cbrnr/pybvrf/pull/3)) by [Clemens Brunner](https://github.com/cbrnr))
+
 ## [0.1.0] · 2026-02-17
 ### ✨ Added
 - Initial release 🎉 (by [Clemens Brunner](https://github.com/cbrnr))
--- src/pybvrf/pybvrf.py
+++ src/pybvrf/pybvrf.py
@@ -6,6 +6,7 @@
 
 import json
 import re
+from importlib.resources import files
 from pathlib import Path
 
 import jsonschema
@@ -120,7 +121,7 @@ def read_bvrf_header(fname):
     with open(fname, encoding="utf-8-sig") as f:
         header = json.load(f)
 
-    with open(Path(__file__).parent / "BVRFHeader-1.0.0.json") as f:
+    with files("pybvrf").joinpath("BVRFHeader-1.0.0.json").open() as f:
         schema = json.load(f)
     try:
         jsonschema.validate(instance=header, schema=schema)
