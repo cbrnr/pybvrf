02d7dd6886f3d5f9eb4c60e39bce7bcc84b72cb2
Clemens Brunner <clemens.brunner@gmail.com>
2026-04-14 16:51:35 +0200

    Lazy import read_raw_bvrf

--- src/pybvrf/__init__.py
+++ src/pybvrf/__init__.py
@@ -7,15 +7,21 @@ from importlib.metadata import PackageNotFoundError, version
 from pybvrf.pybvrf import read_bvrf, read_bvrf_header
 from pybvrf.utils import split_participants
 
-try:
-    from pybvrf.mne_io import read_raw_bvrf
-except ImportError:
 
-    def read_raw_bvrf(*args, **kwargs):
-        raise ImportError(
-            "MNE-Python is required to use read_raw_bvrf(). Install pybvrf with the "
-            "'mne' extra (pybvrf[mne])."
-        )
+def __getattr__(name):
+    if name == "read_raw_bvrf":
+        try:
+            from pybvrf.mne_io import read_raw_bvrf
+        except ImportError:
+
+            def read_raw_bvrf(*args, **kwargs):
+                raise ImportError(
+                    "MNE-Python is required to use read_raw_bvrf(). Install pybvrf with"
+                    " the 'mne' extra (pybvrf[mne])."
+                )
+
+        return read_raw_bvrf
+    raise AttributeError(f"module 'pybvrf' has no attribute {name!r}")
 
 
 try:
